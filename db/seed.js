a15b486cfa08c5583086b1c8f05cd5679694365e
Emmanuel Asu <mannyasu2@gmail.com>
2019-02-27 14:00:05 -0500

    added seed file

---
+++ db/seed.js
@@ -0,0 +1,19 @@
+const Post = require("../models/index");
+
+Post.remove({}).then(() => {
+  Post.insertMany([
+    {
+      title: "Aerobics meet Sunday",
+      content:
+        "Meeting at the park at 4pm Sunday for aerobics exercises.Welcome to join."
+    },
+    {
+      title: " Three mile walk",
+      content:
+        " Interested in a 3 mile walk? Meet tommorrow at 5pm at the town center"
+    }
+  ]).then(posts => {
+    console.log(`new posts added:\n${posts}`);
+    process.exit();
+  });
+});
